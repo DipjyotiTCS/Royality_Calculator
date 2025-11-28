1dc240130fdc8b7d68f74c745b170946be1de55e
DipjyotiTCS <dipjyoti.som@tcs.com>
2025-11-28 22:04:03 +0530

    Primary check in

---
+++ controllers/calculatorController.py
@@ -0,0 +1,139 @@
+from fastapi import APIRouter
+from models.request_model import Request
+from models.response_model import Response
+from models.sales_data_model import SalesData
+from database.db import get_db_connection
+
+router = APIRouter(prefix="/sales", tags=["Sales"])
+
+#API to perform calculation
+
+@router.post("/calculate")
+def calculateSales(request: Request):
+    
+    #Fetch data based on user and isbn
+    conn = get_db_connection()
+    tabular_sales_data = conn.execute("SELECT * FROM sales_data WHERE isbn=? AND author=?", (request.isbn, request.author)).fetchone()
+    conn.close()
+    
+  # Convert row to SalesData object
+    sales_data = SalesData(
+        title=tabular_sales_data["title"],
+        isbn=tabular_sales_data["isbn"],
+        author=tabular_sales_data["author"],
+        process_date=tabular_sales_data["process_date"],
+        sales_total=tabular_sales_data["sales_total"],
+        sales_canada=tabular_sales_data["sales_canada"],
+        sales_chapter=tabular_sales_data["sales_chapter"],
+        sales_us=tabular_sales_data["sales_us"],
+        sales_foreign=tabular_sales_data["sales_foreign"],
+        sales_high_discount=tabular_sales_data["sales_high_discount"],
+        sales_state_adoption=tabular_sales_data["sales_state_adoption"],
+        sales_sub_us=tabular_sales_data["sales_sub_us"],
+        sales_sub_foreign=tabular_sales_data["sales_sub_foreign"],
+        sales_sub_trial=tabular_sales_data["sales_sub_trial"]
+    )
+    
+    #Perform calculation
+    canada_old_amount = sales_data.sales_canada * request.royalty_table_canada
+    canada_new_amount = sales_data.sales_canada * request.royalty_latest_canada
+    canada_diff = 0 if canada_old_amount == canada_new_amount else (canada_new_amount - canada_old_amount)
+    
+    chapter_old_amount = sales_data.sales_chapter * request.royalty_table_chapter
+    chapter_new_amount = sales_data.sales_chapter * request.royalty_latest_chapter
+    chapter_diff = 0 if chapter_old_amount == chapter_new_amount else (chapter_new_amount - chapter_old_amount)
+    
+    us_old_amount = sales_data.sales_us * request.royalty_table_us
+    us_new_amount = sales_data.sales_us * request.royalty_latest_us
+    us_diff = 0 if us_old_amount == us_new_amount else (us_new_amount - us_old_amount)
+    
+    foreign_old_amount = sales_data.sales_foreign * request.royalty_table_foreign
+    foreign_new_amount = sales_data.sales_foreign * request.royalty_latest_foreign
+    foreign_diff = 0 if foreign_old_amount == foreign_new_amount else (foreign_new_amount - foreign_old_amount)
+    
+    
+    highDiscout_old_amount = sales_data.sales_high_discount * request.royalty_table_high_discount
+    highDiscout_new_amount = sales_data.sales_high_discount * request.royalty_latest_high_discount
+    highDiscout_diff = 0 if highDiscout_old_amount == highDiscout_new_amount else (highDiscout_new_amount - highDiscout_old_amount)
+    
+    
+    stateAdoption_old_amount = sales_data.sales_state_adoption * request.royalty_table_state_adoption
+    stateAdoption_new_amount = sales_data.sales_state_adoption * request.royalty_latest_state_adoption
+    stateAdoption_diff = 0 if stateAdoption_old_amount == stateAdoption_new_amount else (stateAdoption_new_amount - stateAdoption_old_amount)
+    
+    subUs_old_amount = sales_data.sales_sub_us * request.royalty_table_sub_us
+    subUs_new_amount = sales_data.sales_sub_us * request.royalty_latest_sub_us
+    subUs_diff = 0 if subUs_old_amount == subUs_new_amount else (subUs_new_amount - subUs_old_amount)
+    
+    subForeign_old_amount = sales_data.sales_sub_foreign * request.royalty_table_sub_foreign
+    subForeign_new_amount = sales_data.sales_sub_foreign * request.royalty_latest_sub_foreign
+    subForeign_diff = 0 if subForeign_old_amount == subForeign_new_amount else (subForeign_new_amount - subForeign_old_amount)
+    
+    subTrial_old_amount = sales_data.sales_sub_trial * request.royalty_table_sub_trial
+    subTrial_new_amount = sales_data.sales_sub_trial * request.royalty_latest_sub_trial
+    subTrial_diff = 0 if subTrial_old_amount == subTrial_new_amount else (subTrial_new_amount - subTrial_old_amount)
+    
+    total_db = canada_old_amount+chapter_old_amount+us_old_amount+foreign_old_amount+highDiscout_old_amount+stateAdoption_old_amount+subUs_old_amount+subForeign_old_amount+subTrial_old_amount
+    
+    total_new = canada_new_amount+us_new_amount+chapter_new_amount+foreign_new_amount+highDiscout_new_amount+stateAdoption_new_amount+subUs_new_amount+subForeign_new_amount+subTrial_new_amount
+    
+    total_diff = canada_diff+chapter_diff+us_diff+foreign_diff+highDiscout_diff+stateAdoption_diff+subUs_diff+subForeign_diff+subTrial_diff
+
+    #Formulate and return the response
+    return Response(title=request.title, 
+                    isbn=request.isbn, 
+                    author=request.author, 
+                    process_date=request.process_date,
+                    royalty_canada_amount=canada_new_amount,
+                    royalty_canada_discr=canada_diff,
+                    royalty_chapter_amount=chapter_new_amount,
+                    royalty_chapter_discr=chapter_diff,
+                    royalty_us_amount=us_new_amount,
+                    royalty_us_discr=us_diff,
+                    royalty_foreign_amount=foreign_new_amount,
+                    royalty_foreign_discr=foreign_diff,
+                    royalty_high_discount_amount=highDiscout_new_amount,
+                    royalty_high_discount_discr=highDiscout_diff,
+                    royalty_state_adoption_amount=stateAdoption_new_amount,
+                    royalty_state_adoption_discr=stateAdoption_diff,
+                    royalty_sub_us_amount=subUs_new_amount,
+                    royalty_sub_us_discr=subUs_diff,
+                    royalty_sub_foreign_amount=subForeign_new_amount,
+                    royalty_sub_foreign_discr=subForeign_diff,
+                    royalty_sub_trial_amount=subTrial_new_amount,
+                    royalty_sub_trial_discr=subTrial_diff,
+                    royalty_total_DB=total_db,
+                    royalty_total_latest=total_new,
+                    royalty_total_disc=total_diff,
+                    royalty_rate_us_response=request.royalty_rate_us_response,
+                    royalty_rate_high_disc_response=request.royalty_rate_high_disc_response,
+                    royalty_rate_sub_response=request.royalty_rate_sub_response,
+                    royalty_rate_canada_response=request.royalty_rate_canada_response,
+                    royalty_rate_chapter_response=request.royalty_rate_chapter_response,
+                    royalty_us_discr_response=request.royalty_us_discr_response,
+                    royalty_rate_foreign_response=request.royalty_rate_foreign_response,
+                    royalty_rate_state_adoptions_response=request.royalty_rate_state_adoptions_response,
+                    royalty_rate_sub_us_response=request.royalty_rate_sub_us_response,
+                    royalty_rate_sub_foreign_response=request.royalty_rate_sub_foreign_response,
+                    royalty_rate_sub_trial_response=request.royalty_rate_sub_trial_response)
+
+# API to add new sales data
+@router.post("/addSalesData")
+def addSalesData(salesData: SalesData):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO sales_data (title, isbn, author, process_date, sales_total, sales_canada, sales_chapter, sales_us, sales_foreign, sales_high_discount, sales_state_adoption, sales_sub_us, sales_sub_foreign, sales_sub_trial) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   (salesData.title, salesData.isbn, salesData.author, salesData.process_date, salesData.sales_total, salesData.sales_canada, salesData.sales_chapter, salesData.sales_us, salesData.sales_foreign, salesData.sales_high_discount, salesData.sales_state_adoption, salesData.sales_sub_us, salesData.sales_sub_foreign, salesData.sales_sub_trial))
+    conn.commit()
+    conn.close()
+    return {"message": "Sales data updated successfully"}
+  
+  
+  
+# API to fetch all sales data
+@router.get("/getAllSalesData")
+def getAllSalesData():
+    conn = get_db_connection()
+    sales_data = conn.execute("SELECT * FROM sales_data").fetchall()
+    conn.close()
+    return [dict(data) for data in sales_data]
---
+++ main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI
+from controllers import calculatorController
+from controllers import authorController
+from controllers import WishdomnextRouterController
+from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+
+app = FastAPI()
+
+# Allow React app origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # React app origin
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, etc.
+    allow_headers=["*"],  # Authorization, Content-Type, etc.
+)
+
+
+# Include routers
+app.include_router(calculatorController.router)
+app.include_router(authorController.router)
+app.include_router(WishdomnextRouterController.router)
+
+# Initialize DB tables if not exist
+conn = sqlite3.connect("local_sales_db.sqlite")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS sales_data (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, isbn INTEGER, author TEXT, process_date TEXT, sales_total INTEGER, sales_canada INTEGER, sales_chapter INTEGER, sales_us INTEGER, sales_foreign INTEGER, sales_high_discount INTEGER, sales_state_adoption INTEGER, sales_sub_us INTEGER, sales_sub_foreign INTEGER, sales_sub_trial INTEGER)")
+cursor.execute("CREATE TABLE IF NOT EXISTS author_data (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, isbn INTEGER, author TEXT, royalty_canada REAL, royalty_chapter REAL, royalty_us REAL, royalty_foreign REAL, royalty_high_discount REAL, royalty_state_adoption REAL, royalty_sub_us REAL, royalty_sub_foreign REAL, royalty_sub_trial REAL, UNIQUE(isbn, author))")
+cursor.execute("CREATE TABLE IF NOT EXISTS author_calculated_data (id INTEGER PRIMARY KEY AUTOINCREMENT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, title TEXT, isbn INTEGER, author TEXT, royalty_canada_amount REAL, royalty_chapter_amount REAL, royalty_us_amount REAL, royalty_foreign_amount REAL, royalty_high_discount_amount REAL, royalty_state_adoption_amount REAL, royalty_sub_us_amount REAL, royalty_sub_foreign_amount REAL, royalty_sub_trial_amount REAL, royalty_total_amount REAL, UNIQUE(isbn, author))")
+conn.commit()
+
+# Insert a sample user if table is empty
+cursor.execute("SELECT COUNT(*) FROM sales_data")
+count = cursor.fetchone()[0]
+if count == 0:
+    cursor.execute("INSERT INTO sales_data (title, isbn, author, process_date, sales_total, sales_canada, sales_chapter, sales_us, sales_foreign, sales_high_discount, sales_state_adoption, sales_sub_us, sales_sub_foreign, sales_sub_trial) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", ("Reading Program", 1234567890123, "John Doe", "23-Nov-2025​", 33750, 2000, 1500, 7000, 3000, 800, 15000, 3600, 700, 150))
+    conn.commit()
+
+cursor.execute("SELECT COUNT(*) FROM author_data")
+count = cursor.fetchone()[0]
+if count == 0:
+    cursor.execute("""INSERT INTO author_data (title, isbn, author, royalty_canada, royalty_chapter, royalty_us, royalty_foreign, royalty_high_discount, royalty_state_adoption, royalty_sub_us, royalty_sub_foreign, royalty_sub_trial) 
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                   ON CONFLICT(isbn, author) DO UPDATE SET
+                        title = excluded.title,
+                        royalty_canada = excluded.royalty_canada,
+                        royalty_chapter = excluded.royalty_chapter,
+                        royalty_us = excluded.royalty_us,
+                        royalty_foreign = excluded.royalty_foreign,
+                        royalty_high_discount = excluded.royalty_high_discount,
+                        royalty_state_adoption = excluded.royalty_state_adoption,
+                        royalty_sub_us = excluded.royalty_sub_us,
+                        royalty_sub_foreign = excluded.royalty_sub_foreign,
+                        royalty_sub_trial = excluded.royalty_sub_trial
+                   """, 
+                   ("Reading Program", 1234567890123, "John Doe", 0.15, 0.1, 0.15, 0.15, 0.075, 0.15, 0.15, 0.15, 0))
+    conn.commit()
+    
+cursor.execute("SELECT COUNT(*) FROM author_calculated_data")
+count = cursor.fetchone()[0]
+if count == 0:
+    cursor.execute("""INSERT INTO author_calculated_data 
+                   (title, isbn, author, royalty_canada_amount, royalty_chapter_amount, royalty_us_amount, royalty_foreign_amount, royalty_high_discount_amount, royalty_state_adoption_amount, royalty_sub_us_amount, royalty_sub_foreign_amount, royalty_sub_trial_amount, royalty_total_amount) 
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ON CONFLICT(isbn, author) DO UPDATE SET
+                        title = excluded.title,
+                        royalty_canada_amount = excluded.royalty_canada_amount,
+                        royalty_chapter_amount = excluded.royalty_chapter_amount,
+                        royalty_us_amount = excluded.royalty_us_amount,
+                        royalty_foreign_amount = excluded.royalty_foreign_amount,
+                        royalty_high_discount_amount = excluded.royalty_high_discount_amount,
+                        royalty_state_adoption_amount = excluded.royalty_state_adoption_amount,
+                        royalty_sub_us_amount = excluded.royalty_sub_us_amount,
+                        royalty_sub_foreign_amount = excluded.royalty_sub_foreign_amount,
+                        royalty_sub_trial_amount = excluded.royalty_sub_trial_amount,
+                        royalty_total_amount = excluded.royalty_total_amount
+                    """, 
+                   ("Reading Program", 1234567890123, "John Doe", 300.0, 300.0, 300.0, 300.0, 300.0, 300.0, 300.0, 300.0, 300.0, 2700))
+    conn.commit()
+
+conn.close()
